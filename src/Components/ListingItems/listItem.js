dde01f091535044d6c5ae4deeba2b84badf6acc3
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-12-08 12:19:08 +0530

    updated ui

---
+++ src/Components/ListingItems/listItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './listItem.css'
+
+let listItem = (props) =>{
+    return(
+        <div className = "Listing">
+            <h1>From Listing</h1>
+        </div>
+    )
+}
+
+export default listItem;
---
+++ src/Components/todoform.js
@@ -0,0 +1,63 @@
+import React , {Component} from 'react'
+import ListItem from '../Components/ListingItems/listItem'
+
+class TodoForm extends Component{
+    constructor(props){
+    super(props);
+        this.state ={
+        items: [],
+        currentItems:{
+            text: '',
+            key: ''
+        }
+        }
+
+    }
+
+    handleInput = (e) =>{
+        this.setState({
+            currentItems:{
+                text: e.target.value,
+                key: Date.now()
+            }
+        })
+    }
+
+    addItem = (e) =>{
+        e.preventDefault();
+        let newItem = this.state.currentItems;
+        console.log(newItem);
+        if(newItem.text === ""){
+            const newItems = [...this.state.items , newItems]
+            this.setState({
+                items: newItems,
+                currentItems:{
+                    text: '',
+                    key:''
+                }
+            })
+        }
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <header>
+                    <form class="todo-form" onSubmit={this.addItem}>
+                    <input 
+                    type="text" 
+                    placeholder="Enter the Text" 
+                    value={this.state.currentItems.text} 
+                    onChange={this.handleInput} />
+                    <button>Add</button>
+                     <ListItem></ListItem>
+                    </form>
+                </header>
+            </div>
+        )
+
+        
+    }
+}
+
+export default TodoForm;
